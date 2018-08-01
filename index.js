4f114123058b741ac09e218c1fe933b14f45825f
Brandan Quinn <Brandan.quinn@isobar.com>
2018-08-01 11:59:26 -0400

    Found logic error - trying to fix.

--- index.js
+++ index.js
@@ -6,24 +6,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'public')));
 
 app.get('/api/info', (req, res, next) => {
-    getLevelObjWrapper().then((csv_string) => { 
-        csv.parse(csv_string, (err, data) => {
-            const levelObj = {};
-            data.forEach((element) => {
-                levelObj[element[0]] = element[1];
-            });
-    
-            res.json(levelObj);
-            
-        }); 
+    getLevelObjWrapper().then((levelObj) => { 
+        res.json(levelObj); 
     });
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- utils/parseCsv.js
+++ utils/parseCsv.js
@@ -2,15 +2,31 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Wrapper functions that returns a Promise for ease of managing async code.
+ */
 const getLevelObjWrapper = () => {
     return new Promise((resolve, reject) => {
         resolve(getLevelObj());
     });
 };
 
-const getLevelObj = () => fs.readFileSync(path.join(__dirname, 'levelsheet.csv'), (err, data) => {
-    if (err) throw err;
-});
+/**
+ * Reads levelsheet.csv and returns a string to be parsed in index.js
+ */
+const getLevelObj = () => { 
+    fs.readFileSync(path.join(__dirname, 'levelsheet.csv'), (err, data) => {
+        if (err) throw err;
+        const csv_string = data.toString();
+        csv.parse(csv_string, (err, data) => {
+            const levelObj = {};
+            data.forEach((element) => {
+                levelObj[element[0]] = element[1];
+            });
+            return levelObj;
+        });
+    });
+};
 
 module.exports = {
     getLevelObjWrapper
